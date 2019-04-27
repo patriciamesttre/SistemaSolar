6ee3fd7a164bbd5868b68ba538d15029bb6939f1
patriciamesttre <patriciamesttre@hotmail.com>
2019-04-26 21:28:08 -0300

    Criando o sol

---
+++ sistemasolar/Assets/Scripts/RotacionaObjeto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionaObjeto : MonoBehaviour
+{
+    public float velocidade = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * velocidade * Time.deltaTime);
+    }
+}
